65a2fd7244736fc0d7113376afadf6b48e60a60b
Ana123tase <anastasedukunda@gmail.com>
2023-09-15 13:53:13 -0700

    Checks for digit (0 through 9)

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,18 @@
 #include "main.h"
+#include "ctype.h"
 /**
- * mul - multiply 2 int
- * @a: first int
- * @b: second int
- * Return: addition of the multiplicattion
+ * _isdigit - checks for a digit (0 through 9).
+ * @c: Char to be checked
+ * Return: 1 if c is a digit, 0 otherwise
  */
-int mul(int a, int b)
+int _isdigit(int c)
 {
-	return (a * b);
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
