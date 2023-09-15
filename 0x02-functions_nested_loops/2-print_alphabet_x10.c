7aba2c6734fb89bf08eb318488996a981f4a23be
Ana123tase <anastasedukunda@gmail.com>
2023-09-15 15:30:54 -0700

    prints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,18 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
 
-
-void print_alphabet_x10(void) {
-    int i;
-    
-    for (i = 0; i < 10; i++) {
-        char lette_ar = 'a';
-        
-        while (lette_ar <= 'z') {
-            _putchar(lette_ar);
-            lette_ar++;
-        }
-        
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
 }
-
