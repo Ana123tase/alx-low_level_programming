551aa04240040d7f296a49a1ba6e201a65786be8
Ana123tase <anastasedukunda@gmail.com>
2023-09-28 05:42:10 -0700

    function that returns 1 if the input integer

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * prime_a calculate a prime number
+ * prime_a - calculate a prime number
  * @a: input
  * @b: divisor
  * Return: Always 0
  */
 int prime_a(int a, int b)
 {
-	if (a <= 1 || (a != b && a %b == 0))
+	if (a <= 1 || (a != b && a % b == 0))
 	{
 		return (0);
 	}
@@ -18,11 +18,11 @@ int prime_a(int a, int b)
 	return (prime_a(a, b + 1));
 }
 /**
- * is_prime_number - detects the prime numbers
+ * is_prime_number - detects if it is the prime numbers
  * @n: input
  * Return: Always 0
  */
-int is_prime_numeber(int n)
+int is_prime_number(int n)
 {
 	return (prime_a(n, 2));
 }
