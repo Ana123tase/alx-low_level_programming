dc6a7bdf1950bccf7aac6967c0f87c22b0c6f494
Ana123tase <anastasedukunda@gmail.com>
2023-12-14 00:59:30 -0800

    Multiple iclusion of main file is avoided

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Compile all source files to object files
 gcc -fPIC -c *.c
-gcc -shared -o liball.so *.o
+
+#Create the dynamic library, excluding the file with the 'main' function
+gcc -shared -o liball.so $(ls *.o | grep -v 'main.o')
