a7bbc469268f304c6f209e21aca8997228df7f11
Ana123tase <anastasedukunda@gmail.com>
2023-12-15 02:22:53 -0800

    creating a dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Compile all source files to object files
 gcc -fPIC -c *.c
-
-#Create the dynamic library, excluding the file with the 'main' function
 gcc -shared -o liball.so $(ls *.o | grep -v 'main.o')
